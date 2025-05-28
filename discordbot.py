8b2dfd0c036797969ffe375e3e29adf959cf20d3
Gamerabbit <jordapaul16@gmail.com>
2025-05-28 15:08:49 +0200

    update

--- discordbot.py
+++ discordbot.py
@@ -14,10 +14,13 @@ class MyClient(discord.Client):
   async def on_message(self, message):
     if message.author == self.user:
       return
-    if message.content.startswith('hello'):
+    if message.content.startswith('hello') and not message.content.endswith('meme'):
       await message.channel.send("Hello"+ " " + message.author.mention)
     if message.content.startswith('meme'):
       await message.channel.send(get_meme()+ " " + message.author.mention)
+    if message.content.startswith('hello') and message.content.endswith('meme'): #if it start with hello and end with meme is send a hey message and a meme
+      await message.channel.send("hello " + message.author.mention + " " +get_meme())
+
 
 intents = discord.Intents.default()
 intents.message_content = True
